815b690fb5803e035c4f381139d68ff3a3c9157c
Ahmad Farag <ahmad.k.farag@gmail.com>
2018-07-15 22:29:59 -0400

    stuff

--- remote/r-jvm/r-loader/src/main/java/org/stacktrace/yo/rjvm/loader/RemoteClassLoader.java
+++ remote/r-jvm/r-loader/src/main/java/org/stacktrace/yo/rjvm/loader/RemoteClassLoader.java
@@ -1,13 +1,16 @@
 package org.stacktrace.yo.rjvm.loader;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.stacktrace.yo.proto.rloader.RLoader;
 import org.stacktrace.yo.rjvm.ws.client.ClassLoaderClient;
 
 import java.net.URI;
 import java.nio.ByteBuffer;
 import java.util.function.Consumer;
 
-public class RemoteClassLoader extends ClassLoader {
-
+public class RemoteClassLoader extends ClassLoader implements ClassLoaderClient.Listener {
+    private static final Logger myLogger = LoggerFactory.getLogger(RemoteClassLoader.class.getSimpleName());
     private final ClassLoaderClient myClient;
 
     public RemoteClassLoader(URI serverUri) {
@@ -16,35 +19,54 @@ public class RemoteClassLoader extends ClassLoader {
 
     public RemoteClassLoader(URI serverUri, ClassLoader parent) {
         super(parent);
-        myClient = new ClassLoaderClient(serverUri);
+        myClient = new ClassLoaderClient(serverUri, this);
     }
 
-    public static final class ClientListener implements ClassLoaderClient.Listener {
-
-        @Override
-        public Consumer<Void> onOpen() {
-            return null;
-        }
-
-        @Override
-        public Consumer<String> onMessage(String message) {
-            return null;
-        }
-
-        @Override
-        public Consumer<ByteBuffer> onMessage(ByteBuffer bytes) {
-            return null;
-        }
-
-        @Override
-        public Consumer<String> onClose(String reason) {
-            return null;
-        }
-
-        @Override
-        public Consumer<Exception> onError(Exception ex) {
-            return null;
-        }
+    @Override
+    public Consumer<Void> onOpen() {
+        return aVoid -> myLogger.debug("[RemoteClassLoader] Opened");
     }
 
+    @Override
+    public Consumer<String> onMessage(String message) {
+        return null;
+    }
+
+    @Override
+    public Consumer<ByteBuffer> onMessage(ByteBuffer bytes) {
+        return byteBuffer -> {
+            try {
+                RLoader.RLoaderMessage.MessageCase messageCase = RLoader.RLoaderMessage
+                        .parseFrom(bytes)
+                        .getMessageCase();
+                switch (messageCase) {
+                    case CONNECTED:
+                    case CLASSLOADED:
+
+                }
+            } catch (Exception e) {
+
+            }
+        };
+    }
+
+    @Override
+    public Class<?> loadClass(String name) throws ClassNotFoundException {
+        return super.loadClass(name);
+    }
+
+    @Override
+    protected Class<?> findClass(String name) throws ClassNotFoundException {
+        return super.findClass(name);
+    }
+
+    @Override
+    public Consumer<String> onClose(String reason) {
+        return null;
+    }
+
+    @Override
+    public Consumer<Exception> onError(Exception ex) {
+        return null;
+    }
 }
